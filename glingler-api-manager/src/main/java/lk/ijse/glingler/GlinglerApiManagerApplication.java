6936ed08b86b00600a5f84db84f6bb21252b57e8
Ravindu Chathuranga <46994483+rchathuranga@users.noreply.github.com>
2020-08-03 13:10:46 +0530

    Initial Commit. Initiate Glingler User Web App (Angular). And Initiate Glingler Api Manager (Spring Boot).

---
+++ glingler-api-manager/src/main/java/lk/ijse/glingler/GlinglerApiManagerApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.glingler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlinglerApiManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlinglerApiManagerApplication.class, args);
+    }
+
+}
---
+++ glingler-user-web-app/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import {async, TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'glingler-user-web-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('glingler-user-web-app');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('glingler-user-web-app app is running!');
+  });
+});
